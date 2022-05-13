4b8f69588d1867dad60bfc9cb643fc1d2b0fc50e
Marco-Gagliano <brazil_94@hotmail.it>
2022-05-13 14:37:34 +0200

    preparazione-pagina

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,18 @@
+<template>
+
+  <footer>
+
+  </footer>
+  
+</template>
+
+<script>
+export default {
+
+  name: 'FooterComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,18 @@
+<template>
+
+  <main>
+
+  </main>
+  
+</template>
+
+<script>
+export default {
+
+  name: 'MainComponents'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
