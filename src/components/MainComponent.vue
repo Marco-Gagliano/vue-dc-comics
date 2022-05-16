506d9803a1a739d64604594d1586f23e15ce180e
Marco-Gagliano <brazil_94@hotmail.it>
2022-05-16 15:25:46 +0200

    preparazione-base-html-card

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,8 +1,52 @@
 <template>
 
   <main>
-    <div class=container>
-      
+    <div class="container">
+      <div class="row row-cols-lg-6 row-cols-sm-3 row-cols-2">
+
+        <div class="col pb-4">
+          <a href="#">
+            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?tok=VdYdJ01" alt="Digital Comics Link">
+          </a>
+          <h4>batman</h4>
+        </div>
+        
+        <div class="col pb-4">
+          <a href="#">
+            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?tok=VdYdJ01" alt="Digital Comics Link">
+          </a>
+          <h4>batman</h4>
+        </div>
+
+        <div class="col pb-4">
+          <a href="#">
+            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?tok=VdYdJ01" alt="Digital Comics Link">
+          </a>
+          <h4>batman</h4>
+        </div>
+
+        <div class="col pb-4">
+          <a href="#">
+            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?tok=VdYdJ01" alt="Digital Comics Link">
+          </a>
+          <h4>batman</h4>
+        </div>
+
+        <div class="col pb-4">
+          <a href="#">
+            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?tok=VdYdJ01" alt="Digital Comics Link">
+          </a>
+          <h4>batman</h4>
+        </div>
+        
+        <div class="col pb-4">
+          <a href="#">
+            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?tok=VdYdJ01" alt="Digital Comics Link">
+          </a>
+          <h4>batman</h4>
+        </div>
+
+      </div>
     </div>
   </main>
   
@@ -20,11 +64,24 @@ export default {
     display: flex;
     align-items: center;
     background-color: #1C1C1C;
-    height: 174px;
+    padding: 40px 0px;
+    
   }
 
-  h1 {
+  .comic-book {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  img {
+    height: 100%;
+    width: 100%;
+    padding: 30px 18px 0px 0px;
+  }
+
+  h4 {
     color: #FFFFFF;
+    margin-top: 20px
   }
 
 </style>
